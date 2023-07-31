fcb55a8c9b2dc4b5681e97f767755adfee97ba61
Nikola Fimić <nikolafimic01@gmail.com>
2023-07-31 23:40:25 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { convertMessageRecieve } from './helpers'
 import { GameActions, GameStatus, IGameState, IPlayer, ITicket } from './models'
 import classNames from 'classnames'
 import { A } from '@solidjs/router'
+import Swal from 'sweetalert2'
 
 const App: Component = () => {
   const ws = new WebSocket('ws://localhost:8080')
@@ -25,24 +26,60 @@ const App: Component = () => {
   })
   const [history, setHistory] = createSignal<string[]>([])
   const [ticketQRCodeImage, setTicketQRCodeImage] = createSignal('')
+  const [timeRemaining, setTimeRemaining] = createSignal(0)
 
   const [betForm, setBetForm] = createStore({
     amount: 0,
     rounds: 0
   })
 
-  const submitBet = () => {
+  const submitBet = (betPerRound: number, numOfRounds: number) => {
     ws.send(
       JSON.stringify({
         type: GameActions.BET,
         data: {
           playerId: playerState().id,
           userBalls: [1, 2, 3, 4, 5, 6],
-          betPerRound: betForm.amount,
-          numOfRounds: betForm.rounds
+          betPerRound,
+          numOfRounds
         } as ITicket
       })
     )
+  }
+
+  const openBetModal = () => {
+    Swal.fire({
+      title: 'Bet',
+      html: `
+        <div class="flex items-center flex-col">
+          <p class="mb-3"> Bet amount per round </p>
+          <input type="number" id="bet-form-bet-per-round" class="text-xl text-center p-3 mb-3 border border-black"/>
+          <p class="mb-3"> Number of rounds </p>
+          <input type="number" id="bet-form-number-of-rounds" class="text-xl text-center p-3 mb-8 border border-black"/>
+        </div>
+        `,
+
+      confirmButtonColor: 'green',
+      confirmButtonText: 'Bet',
+      preConfirm: () => {
+        const betPerRound = Number(
+          (
+            document.getElementById(
+              'bet-form-bet-per-round'
+            ) as HTMLInputElement
+          ).value
+        )
+        const numOfRounds = Number(
+          (
+            document.getElementById(
+              'bet-form-number-of-rounds'
+            ) as HTMLInputElement
+          ).value
+        )
+
+        submitBet(betPerRound, numOfRounds)
+      }
+    })
   }
 
   ws.onopen = () => {
@@ -77,7 +114,23 @@ const App: Component = () => {
         setHistory([...history(), 'you won ' + message.data])
       }
       if (message.type === GameActions.BET_SUCCESS_RESPONSE) {
+        Swal.fire({
+          confirmButtonColor: 'green',
+          html: `
+            <div class="flex flex-col items-center">
+              <h1 class="text-4xl font-bold mb-3"> Your bet was successful </h1>
+              <p class="text-xl"> Scan the QR Code to see your ticket status </p>
+              <img src=${message.data}>
+            </div>
+          `
+        })
         setTicketQRCodeImage(message.data)
+      }
+      if (message.type === GameActions.UPDATE_BALLS) {
+        setPlayingBalls(message.data)
+      }
+      if (message.type === GameActions.TIME_REMAINING) {
+        setTimeRemaining(message.data)
       }
     }
 
@@ -103,7 +156,13 @@ const App: Component = () => {
       <div> {JSON.stringify(ticket())} </div>
       <div> {JSON.stringify(playerState())} </div>
 
-      <div class="text-center flex flex-col items-center">
+      <Show when={gameState()?.status === GameStatus.WAITING_FOR_NEXT_ROUND}>
+        <div> {timeRemaining()} </div>
+      </Show>
+
+      <button onclick={openBetModal}> BET </button>
+
+      {/* <div class="text-center flex flex-col items-center">
         <p class="text-4xl"> BET </p>
         <p> amount </p>
         <input
@@ -127,11 +186,11 @@ const App: Component = () => {
         >
           submit bet
         </button>
-      </div>
+      </div> */}
 
       <img src={ticketQRCodeImage()} />
 
-      <div class="grid grid-cols-6 max-w-lg">
+      <div class=" grid max-w-2xl grid-cols-7">
         <For each={playingBalls()}>
           {(ball) => {
             if (ticket()?.userBalls.includes(ball)) {
@@ -141,7 +200,7 @@ const App: Component = () => {
               <span
                 class={classNames({
                   'bg-green-500 text-white': ticket()?.userBalls.includes(ball),
-                  'flex justify-center items-center pt-8 pb-8 border border-gray-900 text-xl':
+                  ' flex items-center justify-center border border-gray-900 pb-8 pt-8 text-xl':
                     true
                 })}
               >
